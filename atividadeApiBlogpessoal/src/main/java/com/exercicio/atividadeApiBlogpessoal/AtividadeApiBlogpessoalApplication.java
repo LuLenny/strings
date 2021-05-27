4af92a8c383dfbba5c889ad4536ad0a4eb897c2a
LuLenny <lucas.sabido@gmail.com>
2021-05-26 22:53:01 -0300

    atividade de API blogPessoal

---
+++ atividadeApiBlogpessoal/src/main/java/com/exercicio/atividadeApiBlogpessoal/AtividadeApiBlogpessoalApplication.java
@@ -0,0 +1,13 @@
+package com.exercicio.atividadeApiBlogpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeApiBlogpessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeApiBlogpessoalApplication.class, args);
+	}
+
+}
---
+++ atividadeApiBlogpessoal/src/test/java/com/exercicio/atividadeApiBlogpessoal/AtividadeApiBlogpessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicio.atividadeApiBlogpessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeApiBlogpessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
