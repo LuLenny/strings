448ab77e128c7b591a174f6f48dfdc97e9161e69
LuLenny <lucas.sabido@gmail.com>
2021-05-27 14:26:14 -0300

    exercicios dia 27/05

---
+++ refazendoExercicio/src/main/java/com/exercicios/refazendoExercicio/RefazendoExercicioApplication.java
@@ -0,0 +1,13 @@
+package com.exercicios.refazendoExercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefazendoExercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefazendoExercicioApplication.class, args);
+	}
+
+}
