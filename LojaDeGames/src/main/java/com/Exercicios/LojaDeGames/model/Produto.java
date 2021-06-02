1b4df8dd5f9c359158c957a4ab5050f3c6ed000f
LuLenny <lucas.sabido@gmail.com>
2021-06-01 22:15:03 -0300

    exercicio de spring do dia 01/06

---
+++ LojaDeGames/src/main/java/com/Exercicios/LojaDeGames/model/Produto.java
@@ -0,0 +1,57 @@
+package com.Exercicios.LojaDeGames.model;
+
+import java.util.List;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	 
+	@NotNull
+	private String descricao;
+
+	@ManyToOne	
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	
+
+}
